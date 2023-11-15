ea2282fc97e4e1379a4b5df4fe81eabe86f10600
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-11-15 12:09:20 -0800

    initial commit for list and Scrollview App

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [people, setPeople] = useState([
+    { name: 'Tyler', key: 1 },
+    { name: 'Yoshi', key: 2 },
+    { name: 'Mario', key: 3 },
+    { name: 'Luigi', key: 4 },
+    { name: 'Peach', key: 5 },
+    { name: 'Toad', key: 6 },
+    { name: 'Bowser', key: 7 },
+  ])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
     </View>
   );
 }
